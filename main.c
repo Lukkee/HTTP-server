f9280be63a403158bfa1bb3c06dc3132aedb2a2f
Lukas <lukas@holy.nu>
2025-12-22 00:50:23 +0100

    Added response functionality

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 //  Includes
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
 //  Parameters
 #define PORTNUMBER 8000
+
+char *generateResponse ( const char* string ) {
+    size_t len = strlen (string);
+    char *response = malloc(len + 128);
+    if (!response) { perror ("memory allocation for response failed"); exit(1); }
+
+    int written = snprintf (
+        response, len + 128,
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Length: %zu\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n"
+        "%s",
+        len,
+        string
+    );
+
+    if (written < 0 || (size_t)written >= len + 128) {
+        perror ("response formatting failed");
+        free (response);
+        return NULL;
+    }
+
+    return response;
+}
 
 //  Main function
 int main ( void ) {
@@ -15,7 +41,7 @@ int main ( void ) {
     /* Network configuration */
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;          // IPV4
-    addr.sin_port = htons (PORTNUMBER);       // PORT NUMBER
+    addr.sin_port = htons (PORTNUMBER); // PORT NUMBER
     addr.sin_addr.s_addr = INADDR_ANY;  // ANY NETWORKCARD
 
     /* Attach socket to address */
@@ -30,6 +56,14 @@ int main ( void ) {
     recv (client_fd, buffer, sizeof (buffer), 0);
     printf ("Recieved:\n%s\n", buffer);
 
+    /* Respond to request */
+    char *response = generateResponse ("Hello!");
+    send (client_fd, response, strlen (response), 0);
+
+    /* Free memory */
+    free (response);
+
+    /* Close sockets */
     close (client_fd);
     close (server_fd);
     return 0;
